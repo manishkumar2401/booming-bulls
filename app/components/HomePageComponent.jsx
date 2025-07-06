6c2195132720409adf6e4681740ad67708a54762
manish <72023951+manishk2401@users.noreply.github.com>
2025-07-07 00:31:23 +0530

    home

--- app/components/HomePageComponent.jsx
+++ app/components/HomePageComponent.jsx
@@ -1,57 +1,18 @@
 "use client"
 import Head from 'next/head';
-import { motion, useScroll, useTransform, useInView, useSpring } from "framer-motion";
-import { useRef } from 'react';
+import TimelineSteps from "./TimelineSteps"
+import React from 'react';
 
-const timelineData = [
-  {
-    title: "Order Received",
-    description: "We’ve received your order and it’s being processed.",
-  },
-  {
-    title: "Processing",
-    description: "Your items are being carefully packed and labeled.",
-  },
-  {
-    title: "Quality Check",
-    description: "Each item undergoes quality inspection.",
-  },
-  {
-    title: "Dispatched",
-    description: "The shipment has left our warehouse.",
-  },
-  {
-    title: "Delivered",
-    description: "Package delivered to the destination successfully.",
-  },
-];
 
 export default function HomePageComponent() {
-  const containerRef = useRef(null);
-  // const { scrollYProgress } = useScroll({
-  //   target: containerRef,
-  //   offset: ["start 0.3", "end start"],
-  // });
-
-  // const progressHeight = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-
-  const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
-  const opacity = useTransform(scrollYProgress, [0, 1], [0.2, 1]);
-
-  const steps = [
-    { id: 1, title: "Step 1", content: "Description of step 1" },
-    { id: 2, title: "Step 2", content: "Description of step 2" },
-    { id: 3, title: "Step 3", content: "Description of step 3" },
-  ];
 
   return (
     <>
       <Head>
         <title>Landing Page</title>
       </Head>
-      <main className="bg-black">
-        <header className="bg-gray-950 flex items-center justify-center py-16 px-4 font-inter">
+      <main className="h-100vh">
+        <header className="flex items-center bg-black justify-center py-16 px-4 font-inter">
           <div className="text-center max-w-4xl mx-auto">
             {/* "Our Process" button/tag */}
             <div className="inline-block px-4 py-2 mb-2 text-xs font-sm text-[#caa1ca] rounded-full shadow-sm backdrop-blur-sm bg-transparent">
@@ -71,96 +32,10 @@ export default function HomePageComponent() {
           </div>
         </header>
 
-        <motion.div style={{ scale, opacity }}>
-          {steps.map((step) => (
-            <motion.div key={step.id} className="step">
-              <motion.div className="circle" />
-              <div className="content">
-                <h3>{step.title}</h3>
-                <p>{step.content}</p>
-              </div>
-            </motion.div>
-          ))}
-        </motion.div>
-
-        {/* <div className="relative min-h-screen p-10 bg-gray-100" ref={containerRef}>
-          <div className="relative flex max-w-3xl mx-auto">
-            <div className="relative w-1 bg-gray-300 rounded-full mx-4">
-              <motion.div
-                className="absolute top-0 left-0 w-1 bg-green-500 rounded-full origin-top"
-                style={{ height: progressHeight }}
-              />
-            </div>
-
-            <div className="flex flex-col gap-24 pl-6">
-              {timelineData.map((item, index) => (
-                index % 2 ? <TimelineStepRight
-                  key={index}
-                  index={index + 1}
-                  right={true}
-                  title={item.title}
-                  description={item.description}
-                /> : ""
-              ))}
-            </div>
-          </div>
-        </div> */}
+        <TimelineSteps/>
 
 
       </main>
     </>
-  );
-}
-
-
-function TimelineStepRight({ index, title, description }) {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, margin: "-100px" });
-
-  return (
-    <motion.div
-      ref={ref}
-      initial={{ opacity: 0, y: 50 }}
-      animate={isInView ? { opacity: 1, y: 0 } : {}}
-      transition={{ duration: 0.5, ease: "easeOut" }}
-      className="flex items-start gap-6"
-    >
-      <motion.div
-        className={`w-10 h-10 rounded-full flex items-center justify-center font-bold text-white text-sm transition-all duration-300 ${isInView ? "bg-green-600" : "bg-gray-400"
-          }`}
-      >
-        {index}
-      </motion.div>
-      <div>
-        <h3 className={"text-lg font-semibold "}>{title}</h3>
-        <p className="text-gray-600 text-sm">{description}</p>
-      </div>
-    </motion.div>
-  );
-}
-
-function TimelineStepLeft({ index, title, description }) {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, margin: "-100px" });
-
-  return (
-    <motion.div
-      ref={ref}
-      initial={{ opacity: 0, y: 50 }}
-      animate={isInView ? { opacity: 1, y: 0 } : {}}
-      transition={{ duration: 0.5, ease: "easeOut" }}
-      className="flex items-start gap-6"
-    >
-      <motion.div
-        className={`w-10 h-10 rounded-full flex items-center justify-center font-bold text-white text-sm transition-all duration-300 ${isInView ? "bg-green-600" : "bg-gray-400"
-          }`}
-      >
-        {index}
-      </motion.div>
-      <div>
-        <h3 className={"text-lg font-semibold "}>{title}</h3>
-        <p className="text-gray-600 text-sm">{description}</p>
-      </div>
-    </motion.div>
   );
 }
---
+++ app/components/TimelineSteps.jsx
@@ -0,0 +1,85 @@
+"use client"
+import React, { useRef } from 'react';
+import { motion, useScroll, useTransform, useInView } from 'framer-motion';
+const steps = [
+    { id: 1, scrollValue: 0, title: 'Step 1', subtitle: 'Register your account' },
+    { id: 2, scrollValue: 0.25, title: 'Step 2', subtitle: 'Verify your email' },
+    { id: 3, scrollValue: 0.5, title: 'Step 3', subtitle: 'Complete your profile' },
+    { id: 4, scrollValue: 0.75, title: 'Step 4', subtitle: 'Complete your profile' },
+];
+
+export default function TimelineSteps() {
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start start", "end end"]
+      })
+    return (
+        <div ref={ref} style={{ minHeight:"100%", background:"linear-gradient(186deg, rgb(0 0 0 / 73%), rgb(0 0 0)) 0% 0% / cover, url(/assets/img/home-1.webp)", backgroundSize:"cover", backgroundPosition: "center", backgroundRepeat: "no-repeat"}}>
+            {steps.map((el, i) => {
+                return <Step key={i}
+                    index={i}
+                    title={el.title}
+                    scrollValue={el.scrollValue}
+                    subtitle={el.subtitle}
+                    scrollY={scrollYProgress} />
+            })}
+        </div>
+    )
+}
+
+
+const Step = ({ index, title, subtitle, scrollY, scrollValue }) => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+    const height = useTransform(scrollY, [scrollValue, (scrollValue + 0.25)], ["0%", "100%"]);
+    return (
+        (index + 1) % 2 === 0 ? <div ref={ref} className="flex w-full text-white h-75 pb-18">
+            {/* Left Section */}
+            <div className="w-1/2 order-3"></div>
+            {/* Center Section */}
+            <div className="w-[1px] relative flex justify-center items-start order-2">
+                <div className="absolute top-15 h-full w-1 bg-gray-200 rounded overflow-hidden shadow-md">
+                    <motion.div
+                        className="w-full bg-blue-500 rounded"
+                        style={{ height }}
+                    />
+                </div>
+                <div className="absolute text-center">
+                    <div className="text-4xl font-bold">{index + 1}</div>
+                </div>
+            </div>
+            <motion.div
+                initial={{ opacity: 0, y: 30 }}
+                animate={isInView ? { opacity: 1, y: 0 } : {}}
+                transition={{ duration: 0.6 }}
+                className="w-1/2 flex flex-col items-end pr-8 order-1">
+                <span className="text-gray-400">{title}</span>
+                <h2 className="text-2xl font-semibold">{subtitle}</h2>
+            </motion.div>
+        </div> : <div ref={ref} className="flex w-full text-white h-75 pb-18">
+            {/* Left Section */}
+            <div className="w-1/2"></div>
+            {/* Center Section */}
+            <div className="w-[1px] relative flex justify-center items-start">
+                <div className="absolute top-15 h-full w-1 bg-gray-200 rounded overflow-hidden shadow-md">
+                    <motion.div
+                        className="w-full bg-blue-500 rounded"
+                        style={{ height }}
+                    />
+                </div>
+                <div className="absolute text-center">
+                    <div className="text-4xl font-bold">{index + 1}</div>
+                </div>
+            </div>
+            <motion.div
+                initial={{ opacity: 0, y: 30 }}
+                animate={isInView ? { opacity: 1, y: 0 } : {}}
+                transition={{ duration: 0.6 }}
+                className="w-1/2 flex flex-col items-start md:items-start pl-8">
+                <span className="text-gray-400">{title}</span>
+                <h2 className="text-2xl font-semibold">{subtitle}</h2>
+            </motion.div>
+        </div>
+    );
+}
